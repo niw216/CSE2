b7b1ac85e8e37bb827212968c60224c078d20790
niw216 <niw216@lehigh.edu>
2014-08-29 14:55:28 +0000

    \"first draft\" of hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+//Nick Watanabe
+//hw01
+//Welcome Class Java Program
+//
+//  first compile program
+//  javac WelcomeClass.java
+//  run program
+//  java WelcomeClass
+
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[ ] args) {
+        
+        //print the statement
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-n--i--w--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v ");
+        System.out.println("I went fishing in Baja California this summer and caught my first sailfish!");
+        
+        //test/
+        
+    }
+}
